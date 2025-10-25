88056f65021d19b00cca1a25dc85ca91d18e53cd
theM00n21 <modspat@gmail.com>
2025-10-24 20:35:35 -0600

    Update ejemplo.py

---
+++ ejemplo/ejemplo.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# Título de la aplicación
+st.title('Sumadora de dos numeros' )
+
+# Obtener el primer número del usuario
+numero1 = st.number_input('Ingresa el primer numero', value=0)
+
+# Obtener el segundo número del usuario
+numero2 = st.number_input('Ingresa el segundo numero', value=0)
+
+# Realizar la suma
+suma = numero1 + numero2
+
+# Mostrar el resultado
+st.write(f'La suma de {numero1} + {numero2} es: {suma}' )
